35c6824bb15f0e3cfb87e0e5621da2f6d50e5e6e
Monica Li <monicali@live.ca>
2020-11-05 00:13:10 +0000

    added crtl-c exit

--- play.js
+++ play.js
@@ -1,4 +1,28 @@
 const connect = require('./client');
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput)
+  return stdin;
+}
+
+const handleUserInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  
+  stdin.on('data', (key) => {
+    process.stdout.write('.');
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  
+}
+
 console.log('Connecting ...');
 connect();
+setupInput();
